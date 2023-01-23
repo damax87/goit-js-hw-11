473e37819e869801d13a1a1c59bf6343b9769629
damax87 <migunoff87@gmail.com>
2023-01-24 00:32:34 +0200

    3 commit

--- src/js/index.js
+++ src/js/index.js
@@ -21,10 +21,54 @@ refs.loadMoreBtn.addEventListener('click', onLoadMore);
 function onSearch(e) {
     e.preventDefault();
 
-   searchImageService.query = e.currentTarget.elements.query.value;
-   searchImageService.fetchArticles();
+   searchImageService.query = document.querySelector('[name="searchQuery"]').value.trim();
+   searchImageService.resetPage();
+   searchImageService.fetchHits().then(hits => {
+    if (hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        clearGallery();
+        appendHitsMarkup(hits);
+        
+        Notiflix.Notify.success(
+          `Hooray! We found ${hits.length} images.`
+        )};
+   
+    
+   
+   });
 }
 
 function onLoadMore() {
-    searchImageService.fetchArticles();
+    searchImageService.fetchHits().then(appendHitsMarkup);
+}
+
+function appendHitsMarkup(hits) {
+    const murkup = hits.map(hit => {
+        return `<div class="photo-card">
+        <a href="${hit.largeImageURL}"><img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes ${hit.likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views ${hit.views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments ${hit.comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads ${hit.downloads}</b>
+          </p>
+        </div>
+      </div>`;
+    })
+    .join('');
+    refs.gallery.innerHTML += murkup;
+}
+
+function clearGallery() {
+    refs.gallery.innerHTML = '';
 }
--- src/js/search-image.js
+++ src/js/search-image.js
@@ -1,15 +1,40 @@
 export default class SearchImageService {
     constructor() {
         this.searchQuery = '';
+        this.page = 1;
     }
 
-    fetchArticles() {
-        console.log(this);
-        const url = `https://pixabay.com/api/?key=33041546-26ac1afd8f063f5e8fcd45f42&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`;
+    async fetchHits() {
+        const url = `https://pixabay.com/api/?key=33041546-26ac1afd8f063f5e8fcd45f42&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
 
-        fetch(url)
-        .then(r => r.json())
-        .then(console.log);
+       return await fetch(url)
+        .then(async responce => {
+            if(!responce.ok) {
+                if(responce.status === 404) {
+                    return [];
+                }
+                throw new Error(responce.status);
+            }
+            return await responce.json();    
+            })
+            .then(data => {
+       
+                this.incrementPage();
+    
+                return data.hits;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }
+            
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
     }
 
     get query() {
